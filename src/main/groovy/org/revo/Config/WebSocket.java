520b96911e6425f05453aa41b0baaa54c7c802ad
ashraf <ashraf1abdelrasool@gmail.com>
2016-11-06 03:00:07 +0200

    solved

--- src/main/groovy/org/revo/Config/WebSocket.java
+++ src/main/groovy/org/revo/Config/WebSocket.java
@@ -1,16 +1,20 @@
 package org.revo.Config;
 
 import org.springframework.context.annotation.Configuration;
+import org.springframework.core.annotation.Order;
 import org.springframework.messaging.simp.config.MessageBrokerRegistry;
 import org.springframework.web.socket.config.annotation.AbstractWebSocketMessageBrokerConfigurer;
 import org.springframework.web.socket.config.annotation.EnableWebSocketMessageBroker;
 import org.springframework.web.socket.config.annotation.StompEndpointRegistry;
+
+import static org.springframework.core.Ordered.HIGHEST_PRECEDENCE;
 
 /**
  * Created by ashraf on 06/09/16.
  */
 @Configuration
 @EnableWebSocketMessageBroker
+@Order(HIGHEST_PRECEDENCE + 50)
 public class WebSocket extends AbstractWebSocketMessageBrokerConfigurer {
 
 
@@ -25,4 +29,6 @@ public class WebSocket extends AbstractWebSocketMessageBrokerConfigurer {
         registry.enableSimpleBroker("/topic");
 //        registry.enableStompBrokerRelay("/topic/")
     }
+
+//see if you will use jwt http://stackoverflow.com/questions/30887788/json-web-token-jwt-with-spring-based-sockjs-stomp-web-socket
 }
