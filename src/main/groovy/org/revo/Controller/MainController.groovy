2ccdbd95afe34e3f93af979c07609c78425c5482
ashraf <ashraf1abdelrasool@gmail.com>
2016-11-10 13:12:36 +0200

    solved

--- src/main/groovy/org/revo/Controller/MainController.groovy
+++ src/main/groovy/org/revo/Controller/MainController.groovy
@@ -5,13 +5,15 @@ import org.revo.Service.UserService
 import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.cache.annotation.Cacheable
 import org.springframework.http.ResponseEntity
-import org.springframework.messaging.simp.user.SimpUserRegistry
+import org.springframework.messaging.handler.annotation.MessageMapping
+import org.springframework.messaging.simp.SimpMessageSendingOperations
 import org.springframework.stereotype.Controller
 import org.springframework.validation.BindingResult
 import org.springframework.web.bind.annotation.RequestBody
 import org.springframework.web.bind.annotation.RequestMapping
 
 import javax.validation.Valid
+import java.security.Principal
 
 /**
  * Created by ashraf on 8/17/2016.
@@ -31,8 +33,8 @@ class MainController {
                     .groupBy({ it.field })
             return ResponseEntity.badRequest().body(collect)
         }
-        if (userService.findByUsername(user.username).present) return ResponseEntity.badRequest().body([username: [[field:"username",defaultMessage: "please change your username"]]])
-        if (userService.findByPhone(user.phone).present) return ResponseEntity.badRequest().body([phone: [[field:"phone",defaultMessage: "please change your phone"]]])
+        if (userService.findByUsername(user.username).present) return ResponseEntity.badRequest().body([username: [[field: "username", defaultMessage: "please change your username"]]])
+        if (userService.findByPhone(user.phone).present) return ResponseEntity.badRequest().body([phone: [[field: "phone", defaultMessage: "please change your phone"]]])
         else {
             String pass = user.password
             userService.create(user)
@@ -40,5 +42,17 @@ class MainController {
         }
     }
 
+    @MessageMapping("/hello")
+    public void greeting(Message message, Principal principal) throws Exception {
+        operations.convertAndSendToUser(principal.name,"/topic/greetings", message)
+
+    }
+    @Autowired
+    SimpMessageSendingOperations operations
+
+}
+
+class Message {
+    String content
 
 }
